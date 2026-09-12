db59c75704d40ee19f79c2a283d7cbb9c173e57f
Артём <Artemkn212@gmail.com>
2026-09-12 18:05:54 +0300

    /

--- Н12/START_VALEVO_HIDDEN.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "" wscript.exe //B "%~dp0START_VALEVO_HIDDEN.vbs"
-exit
--- Н12/keyboards/profile_menu.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import (
-    InlineKeyboardMarkup,
-    InlineKeyboardButton
-)
-
-profile_menu = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="✏️ Изменить ник", callback_data="change_nick")],
-])
